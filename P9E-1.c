658bf0c712a0589ba24efb288e7b157b192059a0
JANSLEY FELIPE MOREIRA BARBOSA <jansley.fmb@puccampinas.edu.br>
2017-10-31 19:18:27 -0200

    Concertar exercicío 2 da prática 9

---
+++ P9E-1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int QP(int a){
+    float result = sqrt(a);
+    int aux = result;
+    if(aux == result){
+        return 1;
+    }
+
+}
+
+int main(){
+    int number, prova = 0;
+
+    printf("Digite um numero inteiro positivo\n");
+    scanf("%d", &number);
+    if(number > 0){
+        prova = QP(number);
+        if(prova == 1){
+            printf("%d eh um quadrado perfeito\n", number);
+        }
+        else{
+            printf("%d nao eh um quadrado perfeito\n", number);
+        }
+    }
+
+
+    return 0;
+}
---
+++ P9E-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void somadiv(int x, int *y){
+    *y += x;
+}
+
+int mains(){
+    int number, i, soma = 0;
+
+    printf("Digite um numero\n");
+    scanf("%d", &number);
+
+    for(i = 0; i <= number; i++){
+        if(number%i == 0){
+             soma = somadiv(i, &soma);
+        }
+    }
+    printf("A soma dos divisores de %d eh: %d", number, soma);
+
+    return 0;
+}
