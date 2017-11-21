dc950422fa8045098b9ed7fd6d0c140a03a09d16
Jansley Felipe! <jansleyfelipe99@gmail.com>
2017-11-21 11:59:08 -0200

    Add P8E-3 update 1.0

--- Strings/P8E-3.c
+++ Strings/P8E-3.c
@@ -2,30 +2,33 @@
 #include <string.h>
 
 int main(){
-    char s1[50], s2[50];
-    int i, j, x = 0, t1, t2, count = 0;
+    char s1[50], s2[50], s2Original[50];
+    int i, j, count = 0;
 
     printf("Escreva a primeira string\n");
     fflush(stdin);
     gets(s1);
-    t1 = strlen(s1);
     printf("Escreva a segunda string\n");
     fflush(stdin);
     gets(s2);
-    t2 = strlen(s2);
-
-    for(i = 0; i < t1; i++){
-        for(j = 0; j < t2; j++){
-            if(s1[i] == s2[j]){
-                count++;
+    strcpy(s2Original, s2);
+    if(strlen(s1) == strlen(s2)){
+        for(i = 0; s1[i] != '\0'; i++){
+            for(j = 0; s2[j] != '\0'; j++){
+                if(s1[i] == s2[j]){
+                    s2[j] = '7';
+                    count++;
+                }
             }
         }
+        if(count == strlen(s1)){
+            printf("%s eh uma anagrama de %s\n", s1, s2Original);
+        }
+        else{
+            printf("%s nao eh uma anagrama de %s\n", s1, s2Original);
+        }
     }
-    if(count == t1 && count == t2){
-        printf("%s eh uma anagrama de %s\n", s1, s2);
-    }
-    else{
-        printf("%s nao eh uma anagrama de %s\n", s1, s2);
-    }
+    else
+        printf("%s nao eh uma anagrama de %s\n", s1, s2Original);
     return 0;
 }
